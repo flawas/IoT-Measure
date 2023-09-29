838082469ec0e5ca3654c1fa17070d81a15edb68
flawas <32436687+flawas@users.noreply.github.com>
2023-09-29 13:59:32 +0200

    Footer corrected

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div class="col-md-4 d-flex align-items-center">
     <a href="https://flaviowaser.ch" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-        <img src="includes/logo.png" width="30" height="24">
+        <img src="includes/logo.png" width="auto" height="30">
     </a>
     <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Flavio Waser</span>
     </div>
