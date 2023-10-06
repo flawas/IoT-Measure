a57090d09add1ec617bb5d48e39279a4be5ffedb
flawas <32436687+flawas@users.noreply.github.com>
2023-10-06 17:28:04 +0200

    Corrected devices-table.php

--- includes/devices-table.php
+++ includes/devices-table.php
@@ -10,16 +10,17 @@ $devices = mysqli_query($db_connect, "Select * from sensor");
   <div class="row">
   <div class="col-2">
         <input type="date" class="form-control" id="date" name="date" value="">
-        <label for="date" class="form-text" value="">Datum</label>
+        <label for="date" class="form-text" >Datum</label>
     </div>
     <div class="col-2">
-        <input type="number" class="form-control" id="numberOfLogs" name="numberOfLogs">
-        <label for="numberOfLogs" class="form-text" max="100" value="">Anzahl Log-Einträge</label>
+        <input type="number" class="form-control" id="numberOfLogs" name="numberOfLogs" value="">
+        <label for="numberOfLogs" class="form-text" max="100" >Anzahl Log-Einträge</label>
     </div>
     <div class="col-4">
     <select class="form-select" name="device-select" id="device-select">
         <option name="choose" id="choose" value="choose" >Bitte wählen</option>
       <?php
+      
       foreach ($devices as $row){
           ?><option name="<?php echo $row['dev_id']; ?>" value="<?php echo $row['dev_id']; ?>" id="<?php echo $row['dev_id']; ?>"><?php echo $row['dev_place']; ?></option><?php 
       }
@@ -40,38 +41,34 @@ $dev_id = $_GET['device-select'];
 $date = $_GET['date'];
 $numberOfLogs = $_GET['numberOfLogs'];
 
-$sqlResponses = mysqli_query($db_connect, "SELECT * FROM data where datetime like '$date%' and dev_id='$dev_id' order by id DESC LIMIT $numberOfLogs");
+if($dev_id != null && $date != null && $numberOfLogs != null) {
+  $sqlResponses = mysqli_query($db_connect, "SELECT * FROM data where datetime like '$date%' and dev_id='$dev_id' order by id DESC LIMIT $numberOfLogs");
+};
 
 ?>
 
-<table class="table table-hover table-sm">
+<table class="table table-hover table-sm text-center">
   <thead>
     <tr>
       <th>#</th>
       <th>DB Zeitstempel</th>
       <th>App ID</th>
-      <th>TTN Zeitstempel</th>
       <th>Device ID</th>
-      <th>Gateway ID</th>
-      <th>Gateway RSSI</th>
-      <th>Gateway SNR</th>
-      <th>Device Raw Payload</th>
-      <th>Device Value 1</th>
-      <th>Device Value 2</th>
-      <th>Device Value 3</th>
-      <th>Device Value 4</th>
+      <th>Device Value 1 (Temp.)</th>
+      <th>Device Value 2 (Temp.)</th>
+      <th>Device Value 3 (Hum.)</th>
+      <th>Device Value 4 (Batt.)</th>
     </tr>
   </thead>
   <tbody>
-    
 
 <?php
 foreach ($sqlResponses as $sqlResponse){
-    ?> 
-    <tr><th scope="row"><?php echo $sqlResponse['id'];?></th><td><?php echo $sqlResponse['app_id'];?></td><td><?php echo $sqlResponse['dev_id'];?></td><td><?php echo $sqlResponse['ttn_timestamp'];?></td><th><?php echo $sqlResponse['gtw_id'];?></th>
-    <td><?php echo $sqlResponse['gtw_rssi'];?></td><td><?php echo $sqlResponse['gtw_snr'];?></td><td><?php echo $sqlResponse['dev_raw_payload'];?></td><th><?php echo $sqlResponse['dev_value_1'];?></th><td><?php echo $sqlResponse['dev_value_2'];?></td>
-    <td><?php echo $sqlResponse['dev_value_2'];?></td><td><?php echo $sqlResponse['dev_value_3'];?></td><td><?php echo $sqlResponse['dev_value_4'];?></td></tr>
-    <?php 
+  ?> 
+  <tr><th><?php echo $sqlResponse['id'];?></th><td><?php echo $sqlResponse['datetime'];?></td><td><?php echo $sqlResponse['app_id'];?></td>
+  <td><?php echo $sqlResponse['dev_id'];?></td><td><?php echo $sqlResponse['dev_value_1'];?></td>
+  <td><?php echo $sqlResponse['dev_value_2'];?></td><td><?php echo $sqlResponse['dev_value_3'];?></td><td><?php echo $sqlResponse['dev_value_4'];?></td></tr>
+  <?php 
 }
 
 ?>
@@ -89,8 +86,6 @@ function checkInput() {
     }
     return true;
 }
-
-
 
 const urlParams = new URL(window.location.toLocaleString()).searchParams;
 var numberOfLogs = urlParams.get('numberOfLogs');
