13a3dcc2cefd3ae1ff0af77b81d7b60a6e13f3ab
flawas <32436687+flawas@users.noreply.github.com>
2023-10-23 22:15:15 +0200

    Updated chart-daily.php

--- includes/chart-daily.php
+++ includes/chart-daily.php
@@ -1,4 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+
 <?php 
 
 $db_connect = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME) or die(mysql_error());
@@ -39,18 +41,21 @@ $devices = mysqli_query($db_connect, "Select * from sensor");
 $dev_id = $_GET['device-select'];
 $today = $_GET['date'];
 
-$avgTemp = array();
-$avgHumidity = array();
-$hours = array("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
-
-foreach($hours as $hour){
-    $searchLike = $today." ".$hour."%";
-    $sqlResponse = mysqli_query($db_connect, "SELECT round(avg(dev_value_2),2) as avg_temp, round(avg(dev_value_3),2) as avg_hum FROM data where datetime like '$searchLike' and dev_id='$dev_id'");
-    $mysql_row = mysqli_fetch_array($sqlResponse);
-
-    $avgTemp[$hour] = $mysql_row["avg_temp"];
-    $avgHumidity[$hour] = $mysql_row["avg_hum"];
+$query = mysqli_query($db_connect, "SELECT datetime, dev_value_2 as temp1, dev_value_1 as temp2, dev_value_3 as hum FROM data where datetime like '2023-10-05%' and dev_id='$dev_id'");
+if (mysqli_num_rows($query) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($query)) {
+    $temps1 = $temps1 . $row["temp1"] . ",";
+    $temps2 = $temps2 . $row["temp2"] . ",";
+    $hum = $hum . $row["hum"] . ",";
+    $time = explode(" ",$row["datetime"]);
+    $dates = $dates  ."'" . $time[1] . "'" . ",";
+  }
 }
+$finaltemps1 =  rtrim($temps1, ", ");
+$finaltemps2 =  rtrim($temps2, ", ");
+$finalhum =  rtrim($hum, ", ");
+$finaldates = rtrim($dates, ", ");
 ?>
 
 <script>
@@ -85,28 +90,34 @@ if(device != null) {
   new Chart(ctx, {
     type: 'line',
     data: {
-      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+      labels: [<?php echo $finaldates; ?>],
       datasets: [{
-        label: 'Durchschnitt Temperatur',
-        data: [<?php echo $avgTemp['00'];?>,<?php echo $avgTemp['01'];?>,<?php echo $avgTemp['02'];?>,<?php echo $avgTemp['03'];?>,<?php echo $avgTemp['04'];?>,<?php echo $avgTemp['05'];?>,<?php echo $avgTemp['06'];?>,<?php echo $avgTemp['07'];?>, 
-                <?php echo $avgTemp['08'];?>,<?php echo $avgTemp['09'];?>,<?php echo $avgTemp['10'];?>,<?php echo $avgTemp['11'];?>,<?php echo $avgTemp['12'];?>,<?php echo $avgTemp['13'];?>,<?php echo $avgTemp['14'];?>,<?php echo $avgTemp['15'];?>, 
-                <?php echo $avgTemp['16'];?>,<?php echo $avgTemp['17'];?>,<?php echo $avgTemp['18'];?>,<?php echo $avgTemp['19'];?>,<?php echo $avgTemp['20'];?>,<?php echo $avgTemp['21'];?>,<?php echo $avgTemp['22'];?>,<?php echo $avgTemp['23'];?>],
+        label: 'Temperatur 1',
+        data: [<?php echo $finaltemps1;?>],
         borderWidth: 1
-      }, 
-      {
-        label: 'Durchschnitt Luftfeuchtigkeit',
-        data: [<?php echo $avgHumidity['00'];?>,<?php echo $avgHumidity['01'];?>,<?php echo $avgHumidity['02'];?>,<?php echo $avgHumidity['03'];?>,<?php echo $avgHumidity['04'];?>,<?php echo $avgHumidity['05'];?>,<?php echo $avgHumidity['06'];?>,<?php echo $avgHumidity['07'];?>, 
-                <?php echo $avgHumidity['08'];?>,<?php echo $avgHumidity['09'];?>,<?php echo $avgHumidity['10'];?>,<?php echo $avgHumidity['11'];?>,<?php echo $avgHumidity['12'];?>,<?php echo $avgHumidity['13'];?>,<?php echo $avgHumidity['14'];?>,<?php echo $avgHumidity['15'];?>, 
-                <?php echo $avgHumidity['16'];?>,<?php echo $avgHumidity['17'];?>,<?php echo $avgHumidity['18'];?>,<?php echo $avgHumidity['19'];?>,<?php echo $avgHumidity['20'];?>,<?php echo $avgHumidity['21'];?>,<?php echo $avgHumidity['22'];?>,<?php echo $avgHumidity['23'];?>],
+      },{
+        label: 'Temperatur 2',
+        data: [<?php echo $finaltemps2;?>],
+        borderWidth: 1
+      }, {
+        label: 'Luftfeuchtigkeit',
+        data: [<?php echo $finalhum;?>],
         borderWidth: 1
       }]
     },
     options: {
-      scales: {
+      responsive: true,
+      x: {
+        type: 'time',
+        time: {
+          // Luxon format string
+          tooltipFormat: 'H:mm'
+        },
+      }, scales: {
         y: {
           beginAtZero: true
         }
       }
-    }
+    },
   });
 </script>
