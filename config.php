59325a30f5854929f8f27b00ca2013df45bd69f4
flawas <32436687+flawas@users.noreply.github.com>
2023-11-12 14:53:51 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,9 @@
 <?php
 
 // Database
-define('DATABASE_USERNAME', ''); 
+define('DATABASE_USERNAME', 'root'); 
 define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME', '');
-define('DATABASE_HOST', '');
+define('DATABASE_NAME', 'flawasch_iot');
+define('DATABASE_HOST', 'localhost');
 define('WRITE_LOG', true); //Enable log.txt File
-
-
-// Slack
-define('SLACK_ENABLED', FALSE);
-define('SLACK_WEBHOOK', '');
-define('SLACK_HUMIDITY_THREESHOLD', 55);
-define('SLACK_TEMPERATURE_THREESHOLD', 20);
-
 ?>
