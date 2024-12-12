0491d2d1ab36bab7a95be44742cc0980cb889467
flawas <32436687+flawas@users.noreply.github.com>
2024-12-12 15:28:28 +0100

    Revert chart-daily.php

--- includes/chart-daily.php
+++ includes/chart-daily.php
@@ -1,10 +1,12 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+
 
 <?php
 
-$db_connect = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME) or die(mysqli_error($db_connect));
-$devices = mysqli_query($db_connect, "SELECT * FROM sensor");
+$db_connect = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME) or die(mysql_error());
+$devices = mysqli_query($db_connect, "Select * from sensor");
 
 ?>
 <div class="container text-center">
@@ -16,14 +18,14 @@ $devices = mysqli_query($db_connect, "SELECT * FROM sensor");
             </div>
             <div class="col-8">
                 <select class="form-select" name="device-select" id="device-select">
-                    <option name="choose" id="choose" value="choose">Bitte wählen</option>
+                    <option name="choose" id="choose" value="choose" >Bitte wählen</option>
                     <?php
                     foreach ($devices as $row){
                         ?><option name="<?php echo $row['dev_id']; ?>" value="<?php echo $row['dev_id']; ?>" id="<?php echo $row['dev_id']; ?>"><?php echo $row['dev_place']; ?></option><?php
                     }
                     ?>
                 </select>
-                <label for="device-select" id="device-select-label" class="form-text">Sensor</label>
+                <label for="device-select" id="device-select-label"class="form-text">Sensor</label>
             </div>
             <div class="col-2">
                 <input class="btn btn-secondary" type='submit' name="submit" value='Anzeigen'>
@@ -47,6 +49,7 @@ $devices = mysqli_query($db_connect, "SELECT * FROM sensor");
     } else {
         document.getElementById('choose').setAttribute('selected', 'selected');
     }
+
 </script>
 
 <div>
@@ -57,9 +60,9 @@ $devices = mysqli_query($db_connect, "SELECT * FROM sensor");
 $dev_id = $_GET['device-select'];
 $today = $_GET['date'];
 
-$query = mysqli_query($db_connect, "SELECT datetime, dev_value_2 as temp1, dev_value_1 as temp2, dev_value_3 as hum, dev_value_5 as soil FROM data WHERE datetime LIKE '$today%' AND dev_id='$dev_id'");
-$temp1 = $temp2 = $hum = $soil = "";
+$query = mysqli_query($db_connect, "SELECT datetime, dev_value_2 as temp1, dev_value_1 as temp2, dev_value_3 as hum, dev_value_5 as soil FROM data where datetime like '$today%' and dev_id='$dev_id'");
 if (mysqli_num_rows($query) > 0) {
+    // output data of each row
     while($row = mysqli_fetch_assoc($query)) {
         $temp1  .= "{x: '".$row["datetime"]."', y: ".$row["temp1"]."},";
         $temp2  .= "{x: '".$row["datetime"]."', y: ".$row["temp2"]."},";
@@ -72,19 +75,19 @@ $finaltemps2 =  rtrim($temp2, ", ");
 $finalhum =  rtrim($hum, ", ");
 $finalsoil =  rtrim($soil, ", ");
 
-$sensorvalues = mysqli_query($db_connect, "SELECT * FROM sensor WHERE dev_id='$dev_id'");
+$sensorvalues = mysqli_query($db_connect, "Select * from sensor WHERE dev_id='$dev_id'");
 $sensorrow = mysqli_fetch_assoc($sensorvalues);
 
-$forecast = mysqli_query($db_connect, "SELECT datetime, temperature as forecasttemp, humidity as forecasthum FROM data_openmeteo WHERE datetime LIKE '$today%'");
-$forecasttemp = $forecasthum = "";
+$forecast = mysqli_query($db_connect, "SELECT datetime, temperature as forecasttemp, humidity as forecasthum FROM data_openmeteo where datetime like '$today%'");
 if (mysqli_num_rows($forecast) > 0) {
+    // output data of each row
     while($row = mysqli_fetch_assoc($forecast)) {
-        $forecasttemp .= "{x: '".$row["datetime"]."', y: ".$row["forecasttemp"]."},";
+        $forecasttime = explode(" ",$row["datetime"]);
         $forecasthum .= "{x: '".$row["datetime"]."', y: ".$row["forecasthum"]."},";
+        $forecasttemp .= "{x: '".$row["datetime"]."', y: ".$row["forecasttemp"]."},";
     }
+    $finalforecasttemp = rtrim($forecasttemp, ", ");
 }
-$finalforecasttemp = rtrim($forecasttemp, ", ");
-$finalforecasthum = rtrim($forecasthum, ", ");
 ?>
 
 <script>
@@ -99,55 +102,36 @@ $finalforecasthum = rtrim($forecasthum, ", ");
     }
 
     const ctx = document.getElementById('chartDaily');
-    const datasets = [];
-
-    if ('<?php echo $finaltemps1; ?>' !== '') {
-        datasets.push({
-            label: '<?php echo $sensorrow['value_1_name']; ?>',
-            data: [<?php echo $finaltemps1; ?>],
-            borderWidth: 1,
-        });
-    }
-    if ('<?php echo $finaltemps2; ?>' !== '') {
-        datasets.push({
-            label: '<?php echo $sensorrow['value_2_name']; ?>',
-            data: [<?php echo $finaltemps2; ?>],
-            borderWidth: 1,
-        });
-    }
-    if ('<?php echo $finalhum; ?>' !== '') {
-        datasets.push({
-            label: '<?php echo $sensorrow['value_3_name']; ?>',
-            data: [<?php echo $finalhum; ?>],
-            borderWidth: 1,
-        });
-    }
-    if ('<?php echo $finalsoil; ?>' !== '') {
-        datasets.push({
-            label: '<?php echo $sensorrow['value_5_name']; ?>',
-            data: [<?php echo $finalsoil; ?>],
-            borderWidth: 1,
-        });
-    }
-    if ('<?php echo $finalforecasttemp; ?>' !== '') {
-        datasets.push({
-            label: 'Wettervorhersage Temperatur',
-            data: [<?php echo $finalforecasttemp; ?>],
-            borderWidth: 1,
-        });
-    }
-    if ('<?php echo $finalforecasthum; ?>' !== '') {
-        datasets.push({
-            label: 'Wettervorhersage Luftfeuchtigkeit',
-            data: [<?php echo $finalforecasthum; ?>],
-            borderWidth: 1,
-        });
-    }
 
     new Chart(ctx, {
         type: 'line',
         data: {
-            datasets: datasets
+            datasets: [{
+                label: '<?php echo $sensorrow['value_1_name']; ?>',
+                data: [<?php echo $finaltemps1;?>],
+                borderWidth: 1,
+            },{
+                label: '<?php echo $sensorrow['value_2_name']; ?>',
+                data: [<?php echo $finaltemps2;?>],
+                borderWidth: 1,
+            }, {
+                label: '<?php echo $sensorrow['value_3_name']; ?>',
+                data: [<?php echo $finalhum;?>],
+                borderWidth: 1,
+            }, {
+                label: '<?php echo $sensorrow['value_5_name']; ?>',
+                data: [<?php echo $finalsoil;?>],
+                borderWidth: 1,
+            }, {
+                label: 'Wettervorhersage Temperatur',
+                data: [<?php echo $forecasttemp;?>],
+                borderWidth: 1,
+            }, {
+                label: 'Wettervorhersage Luftfeuchtigkeit',
+                data: [<?php echo $forecasthum;?>],
+                borderWidth: 1,
+            }
+            ]
         },
         options: {
             scales: {
